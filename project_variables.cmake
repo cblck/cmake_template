dfcca3ee2cbe41b7e62e9a8b09a95761f4eceeb1
cblck <156492894+cblck@users.noreply.github.com>
2024-01-24 21:02:51 -0800

    Explicitly set cmake to version 3.27.2 for VS2022 compatibility. Reverts previous attempt at cmake compatibility.

--- project_variables.cmake
+++ project_variables.cmake
@@ -1,13 +1,5 @@
 set(PROJ_EXEC1 "intro")
 
-# Disable C++20 module support until CMake, LLVM, and Ninja all get along.
-if(CMAKE_VERSION VERSION_GREATER 3.27.2)
-  if(CMAKE_VERSION VERSION_LESS 3.28.2)
-    message(STATUS "CMake NOT scanning for C++20 modules.")
-    set(CMAKE_CXX_SCAN_FOR_MODULES FALSE)
-  endif()
-endif()
-
 #directories under src that will be added as subdirectories to the project
 set(MY_PROJECT_SUBDIRECTORIES "ftxui_sample;sample_library")
 
