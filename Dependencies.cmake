92ae88c39d3e38fd44a89ccad3229440e4a2d208
cblck <156492894+cblck@users.noreply.github.com>
2024-02-01 19:09:48 -0800

    Changes lefticus/tools#update_build_system to cblck/lefticus-tools#main to resolve IntelliSense bugs.

--- Dependencies.cmake
+++ Dependencies.cmake
@@ -48,7 +48,7 @@ function(myproject_setup_dependencies)
   endif()
 
   if(NOT TARGET tools::tools)
-    cpmaddpackage("gh:lefticus/tools#update_build_system")
+    cpmaddpackage("gh:cblck/lefticus-tools#main")
   endif()
 
 endfunction()
